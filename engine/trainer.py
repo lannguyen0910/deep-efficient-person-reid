7e9e170b8da5178362e9d9707feb992369b0e26d
lannguyen <18120051@student.hcmus.edu.vn>
2021-06-19 06:37:37 +0700

    trainer

---
+++ engine/trainer.py
@@ -0,0 +1,313 @@
+import logging
+
+import torch
+import torch.nn as nn
+from ignite.engine import Engine, Events
+from ignite.handlers import ModelCheckpoint, Timer
+from ignite.metrics import RunningAverage
+
+from metrics.mAP import R1_mAP
+
+global ITER
+ITER = 0
+
+
+def create_supervised_trainer(config, model, optimizer, loss_fn,
+                              device=None):
+    """
+    Factory function for creating a trainer for supervised models
+    Args:
+        model (`torch.nn.Module`): the model to train
+        optimizer (`torch.optim.Optimizer`): the optimizer to use
+        loss_fn (torch.nn loss function): the loss function to use
+        device (str, optional): device type specification (default: None).
+            Applies to both model and batches.
+    Returns:
+        Engine: a trainer engine with supervised update function
+    """
+    if device:
+        if torch.cuda.device_count() > 1:
+            model = nn.DataParallel(model)
+        model.to(device)
+
+    def _update(engine, batch):
+        model.train()
+        img, target = batch
+        img = img.to(device) if torch.cuda.device_count() >= 1 else img
+        target = target.to(
+            device) if torch.cuda.device_count() >= 1 else target
+        score, feat = model(img)
+
+        with torch.cuda.amp.autocast(enabled=config.mixed_precision):
+            amp_scale = torch.cuda.amp.GradScaler()
+            loss = loss_fn(score, feat, target)
+            # print("Total loss is {}".format(
+            #     loss))
+
+        if config.mixed_precision:
+            optimizer.zero_grad()
+            amp_scale.scale(loss).backward()
+            amp_scale.step(optimizer)
+            amp_scale.update()
+
+        else:
+            loss.backward()
+            optimizer.step()
+
+        # compute acc
+        acc = (score.max(1)[1] == target).float().mean()
+        return loss.item(), acc.item()
+
+    return Engine(_update)
+
+
+def create_supervised_trainer_with_center(config, model, center_criterion, optimizer, optimizer_center, loss_fn, center_loss_weight,
+                                          device=None):
+    if device:
+        if torch.cuda.device_count() > 1:
+            model = nn.DataParallel(model)
+        model.to(device)
+
+    def _update(engine, batch):
+        model.train()
+        optimizer.zero_grad()
+        optimizer_center.zero_grad()
+        img, target = batch
+        img = img.to(device) if torch.cuda.device_count() >= 1 else img
+        target = target.to(
+            device) if torch.cuda.device_count() >= 1 else target
+        score, feat = model(img)
+
+        with torch.cuda.amp.autocast(enabled=config.mixed_precision):
+            amp_scale = torch.cuda.amp.GradScaler()
+            loss = loss_fn(score, feat, target)
+            # print("Total loss is {}, center loss is {}".format(
+            #     loss, center_criterion(feat, target)))
+
+        if config.mixed_precision:
+            optimizer.zero_grad()
+            amp_scale.scale(loss).backward()
+            amp_scale.step(optimizer)
+
+        else:
+            loss.backward()
+            optimizer.step()
+
+        for param in center_criterion.parameters():
+            param.grad.data *= (1. / center_loss_weight)
+
+        if config.mixed_precision:
+            amp_scale.step(optimizer_center)
+            amp_scale.update()
+        else:
+            optimizer_center.step()
+
+        # compute acc
+        acc = (score.max(1)[1] == target).float().mean()
+        return loss.item(), acc.item()
+
+    return Engine(_update)
+
+
+def create_supervised_evaluator(model, metrics,
+                                device=None):
+    """
+    Factory function for creating an evaluator for supervised models
+    Args:
+        model (`torch.nn.Module`): the model to train
+        metrics (dict of str - :class:`ignite.metrics.Metric`): a map of metric names to Metrics
+        device (str, optional): device type specification (default: None).
+            Applies to both model and batches.
+    Returns:
+        Engine: an evaluator engine with supervised inference function
+    """
+    if device:
+        if torch.cuda.device_count() > 1:
+            model = nn.DataParallel(model)
+        model.to(device)
+
+    def inference(engine, batch):
+        model.eval()
+        with torch.no_grad():
+            data, pids, camids = batch
+            data = data.to(device) if torch.cuda.device_count() >= 1 else data
+            feat = model(data)
+
+            return feat, pids, camids
+
+    engine = Engine(inference)
+
+    for name, metric in metrics.items():
+        metric.attach(engine, name)
+
+    return engine
+
+
+def do_train(
+        config,
+        model,
+        train_loader,
+        val_loader,
+        optimizer,
+        scheduler,
+        loss_fn,
+        num_query,
+        start_epoch
+):
+    log_period = config.log_period
+    checkpoint_period = config.checkpoint_period
+    eval_period = config.eval_period
+    output_dir = config.output_dir
+    device = config.device
+    epochs = config.num_epochs
+
+    logger = logging.getLogger("reid_baseline.train")
+    logger.info("Start training")
+    trainer = create_supervised_trainer(
+        config, model, optimizer, loss_fn, device=device)
+    evaluator = create_supervised_evaluator(model, metrics={'r1_mAP': R1_mAP(
+        num_query, max_rank=50, feat_norm=config.test_feat_norm)}, device=device)
+    checkpointer = ModelCheckpoint(
+        output_dir, config.model_name, checkpoint_period, n_saved=10, require_empty=False)
+    timer = Timer(average=True)
+
+    trainer.add_event_handler(Events.EPOCH_COMPLETED, checkpointer, {'model': model,
+                                                                     'optimizer': optimizer})
+    timer.attach(trainer, start=Events.EPOCH_STARTED, resume=Events.ITERATION_STARTED,
+                 pause=Events.ITERATION_COMPLETED, step=Events.ITERATION_COMPLETED)
+
+    # average metric to attach on trainer
+    RunningAverage(output_transform=lambda x: x[0]).attach(trainer, 'avg_loss')
+    RunningAverage(output_transform=lambda x: x[1]).attach(trainer, 'avg_acc')
+
+    @trainer.on(Events.STARTED)
+    def start_training(engine):
+        engine.state.epoch = start_epoch
+
+    @trainer.on(Events.EPOCH_STARTED)
+    def adjust_learning_rate(engine):
+        scheduler.step()
+
+    @trainer.on(Events.ITERATION_COMPLETED)
+    def log_training_loss(engine):
+        global ITER
+        ITER += 1
+
+        if ITER % log_period == 0:
+            logger.info("Epoch[{}] Iteration[{}/{}] Loss: {:.3f}, Acc: {:.3f}, Base Lr: {:.2e}"
+                        .format(engine.state.epoch, ITER, len(train_loader),
+                                engine.state.metrics['avg_loss'], engine.state.metrics['avg_acc'],
+                                scheduler.get_lr()[0]))
+        if len(train_loader) == ITER:
+            ITER = 0
+
+    # adding handlers using `trainer.on` decorator API
+    @trainer.on(Events.EPOCH_COMPLETED)
+    def print_times(engine):
+        logger.info('Epoch {} done. Time per batch: {:.3f}[s] Speed: {:.1f}[samples/s]'
+                    .format(engine.state.epoch, timer.value() * timer.step_count,
+                            train_loader.batch_size / timer.value()))
+        logger.info('-' * 10)
+        timer.reset()
+
+    @trainer.on(Events.EPOCH_COMPLETED)
+    def log_validation_results(engine):
+        if engine.state.epoch % eval_period == 0:
+            print('Trainer eval')
+            evaluator.run(val_loader)
+            cmc, mAP, _, _, _, _, _ = evaluator.state.metrics['r1_mAP']
+            logger.info(
+                "Validation Results - Epoch: {}".format(engine.state.epoch))
+            logger.info("mAP: {:.1%}".format(mAP))
+            for r in [1, 5, 10]:
+                logger.info(
+                    "CMC curve, Rank-{:<3}:{:.1%}".format(r, cmc[r - 1]))
+
+    trainer.run(train_loader, max_epochs=epochs)
+
+
+def do_train_with_center(
+        config,
+        model,
+        center_criterion,
+        train_loader,
+        val_loader,
+        optimizer,
+        optimizer_center,
+        scheduler,
+        loss_fn,
+        num_query,
+        start_epoch
+):
+    log_period = config.log_period
+    checkpoint_period = config.checkpoint_period
+    eval_period = config.eval_period
+    output_dir = config.output_dir
+    device = config.device
+    epochs = config.num_epochs
+
+    logger = logging.getLogger("reid_baseline.train")
+    logger.info("Start training")
+    trainer = create_supervised_trainer_with_center(
+        config, model, center_criterion, optimizer, optimizer_center, loss_fn, config.center_loss_weight, device=device)
+    evaluator = create_supervised_evaluator(model, metrics={'r1_mAP': R1_mAP(
+        num_query, max_rank=50, feat_norm=config.test_feat_norm)}, device=device)
+    checkpointer = ModelCheckpoint(
+        output_dir, config.model_name, checkpoint_period, n_saved=10, require_empty=False)
+    timer = Timer(average=True)
+
+    trainer.add_event_handler(Events.EPOCH_COMPLETED, checkpointer, {'model': model,
+                                                                     'optimizer': optimizer,
+                                                                     'center_param': center_criterion,
+                                                                     'optimizer_center': optimizer_center})
+
+    timer.attach(trainer, start=Events.EPOCH_STARTED, resume=Events.ITERATION_STARTED,
+                 pause=Events.ITERATION_COMPLETED, step=Events.ITERATION_COMPLETED)
+
+    # average metric to attach on trainer
+    RunningAverage(output_transform=lambda x: x[0]).attach(trainer, 'avg_loss')
+    RunningAverage(output_transform=lambda x: x[1]).attach(trainer, 'avg_acc')
+
+    @trainer.on(Events.STARTED)
+    def start_training(engine):
+        engine.state.epoch = start_epoch
+
+    @trainer.on(Events.EPOCH_STARTED)
+    def adjust_learning_rate(engine):
+        scheduler.step()
+
+    @trainer.on(Events.ITERATION_COMPLETED)
+    def log_training_loss(engine):
+        global ITER
+        ITER += 1
+
+        if ITER % log_period == 0:
+            logger.info("Epoch[{}] Iteration[{}/{}] Loss: {:.3f}, Acc: {:.3f}, Base Lr: {:.2e}"
+                        .format(engine.state.epoch, ITER, len(train_loader),
+                                engine.state.metrics['avg_loss'], engine.state.metrics['avg_acc'],
+                                scheduler.get_lr()[0]))
+        if len(train_loader) == ITER:
+            ITER = 0
+
+    # adding handlers using `trainer.on` decorator API
+    @trainer.on(Events.EPOCH_COMPLETED)
+    def print_times(engine):
+        logger.info('Epoch {} done. Time per batch: {:.3f}[s] Speed: {:.1f}[samples/s]'
+                    .format(engine.state.epoch, timer.value() * timer.step_count,
+                            train_loader.batch_size / timer.value()))
+        logger.info('-' * 10)
+        timer.reset()
+
+    @trainer.on(Events.EPOCH_COMPLETED)
+    def log_validation_results(engine):
+        if engine.state.epoch % eval_period == 0:
+            evaluator.run(val_loader)
+            cmc, mAP, _ , _, _ , _, _ = evaluator.state.metrics['r1_mAP']
+            logger.info(
+                "Validation Results - Epoch: {}".format(engine.state.epoch))
+            logger.info("mAP: {:.1%}".format(mAP))
+            for r in [1, 5, 10]:
+                logger.info(
+                    "CMC curve, Rank-{:<3}:{:.1%}".format(r, cmc[r - 1]))
+
+    trainer.run(train_loader, max_epochs=epochs)
